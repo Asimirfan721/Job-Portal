07cb9e00661907bd901d891d9f0538135062141d
Asim Irfan <rajputasim721@gmail.com>
2025-04-24 16:13:39 +0500

    @yield and @extends file understood

---
+++ app/Http/Controllers/AccountController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AccountController extends Controller
+{
+    public function Registeration(){
+        return view('front.account.registeration');
+
+    }
+    public function Login(){
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
-
+use App\Http\Controllers\AccountController;
 Route::get('/', function () {
     return view('welcome');
 });
 
 
 Route::get('/home', [HomeController::class, 'index'])->name ('home');
+Route::get('/account/register', [AccountController::class, 'registeration'])->name ('registeration');
