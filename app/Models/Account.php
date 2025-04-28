edd40a00c5301697d2df5b184ef3e4d57b520078
Asim Irfan <rajputasim721@gmail.com>
2025-04-28 14:28:45 +0500

    relatio created between job and account via model

--- app/Models/Account.php
+++ app/Models/Account.php
@@ -3,8 +3,25 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Account extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'confirm_password',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    public function getAuthPassword()
+    {
+        return $this->password;
+    }
 }
