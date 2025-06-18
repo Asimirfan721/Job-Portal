83458e365cca6af3f88d88b750ef0c16e96f7559
Asim Irfan <rajputasim721@gmail.com>
2025-06-18 15:29:20 +0500

    role field added, it's not being stored in database

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,10 +18,12 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    'name',
+    'email',
+    'password',
+    'role', // ✅ include this
+];
+
 
     /**
      * The attributes that should be hidden for serialization.
